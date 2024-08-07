821974c4b1a58f24a1cb662c25e7ee629808c695
lotaxgizmo <lotaoko2@gmail.com>
2024-08-07 16:51:46 +0100

    ff

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -8,7 +8,7 @@ import comingsoon from '../assets/comingsoon.svg'
 
 function Buy() {
     return (
-        <div className='relative  linkbg bg-[#C3FCF1] rounded-[55px] p-5 my-10 border-4 border-black overflow-clip m-4'>
+        <div className='relative  linkbg bg-[#C3FCF1] rounded-[55px] p-5 my-10 border-4 border-black overflow-clip m-4 mb-60'>
 
             <div className="total flex flex-col lg:flex-row justify-center items-center">
 
@@ -18,18 +18,18 @@ function Buy() {
                     <div className="elite p-3 top flex flex-row flex-wrap justify-center items-center">
 
                         <a href="/#" target="_blank" rel="noopener noreferrer">
-                            <img src={cowswap} alt="" className='w-40 lg:w-60 p-1' />
+                            <img src={cowswap} alt="" className='w-32 lg:w-60 p-1' />
                         </a>
 
                         <a href="/#" target="_blank" rel="noopener noreferrer">
-                            <img src={uniswap} alt="" className='w-40 lg:w-60 p-1' />
+                            <img src={uniswap} alt="" className='w-32 lg:w-60 p-1' />
                         </a>
 
                         <a href="/#" target="_blank" rel="noopener noreferrer">
-                            <img src={comingsoon} alt="" className='w-40 lg:w-60 p-1' />
+                            <img src={comingsoon} alt="" className='w-32 lg:w-60 p-1' />
                         </a>
                         <a href="/#" target="_blank" rel="noopener noreferrer">
-                            <img src={comingsoon} alt="" className='w-40 lg:w-60 p-1' />
+                            <img src={comingsoon} alt="" className='w-32 lg:w-60 p-1' />
                         </a>
 
                     </div>
